70ffbc1cf15d70d9d54d6e88c4fb35b525c6d534
diegommppg <diegommppg@gmail.com>
2025-11-11 10:12:22 +0100

    Crear lista con MVC

--- Tema 2/mapas/mapas.js
+++ Tema 2/mapas/mapas.js
@@ -1,3 +1,4 @@
+/*
 //Crear Mapa
 const animales = new Map();
 
@@ -24,4 +25,100 @@ animales.size;
 //Recorrer valores mapa
 for(anim of animales.values()){
     console.log(anim);
+}
+
+//Recorrer claves mapa
+for(animal of animales.keys()){
+    console.log(animal);
+}
+
+//Recorrer claves y valores
+for(animal of animales){
+    console.log("Clave: " + animal[0] + ". Valor: " + animal[1]);
+}
+
+for(animal of animales.keys()){
+    console.log("Clave: " + animal + ". Valor: " + animales.get(animal));
+}
+
+*/
+
+//Modelo
+const animales = new Map([
+    ['Gato', 'miau'],
+    ['Perro', 'guau'],
+    ['Vaca', 'muuuu'],
+    ['Pavo', 'apagaluz'],
+    ['Gochu', 'oink'],
+    ['Serpiente', 'ssssss']
+]);
+
+function anadirDatoModelo(clave, valor){
+    //Comprobar si existe
+    if(animales.has(clave)){
+        console.log("El animal ya existe");
+        return false;
+    }
+
+    animales.set(clave, valor);
+    return true;
+}
+
+function eliminarDatoModelo(clave){
+    //Comprobar si existe
+    if(!animales.has(clave)){
+        return false;
+    }
+
+    animales.delete(clave);
+    return true;
+}
+
+function devolverDatosModelo(){
+    return animales;
+}
+
+//Vista
+let claveAnimalAnadir = document.getElementById('claveAnimal');
+let valorAnimalAnadir = document.getElementById('valorAnimal');
+let claveAnimalEliminar = document.getElementById('eliminarAnimal');
+
+function anadirElementoVista(clave, valor){
+    let li = document.createElement('li');
+    li.textContent = clave + ": " + valor;
+    document.getElementById('listaAnimales').appendChild(li);
+}
+
+function mostrarAlerta(msg){
+    alert(msg);
+}
+
+//Controlador
+function cargarAnimales(){
+    let datos = devolverDatosModelo();
+    for(animal of datos){
+        anadirElementoVista(animal[0], animal[1]);
+    }
+}
+cargarAnimales();
+
+function anadirAnimal(){
+    let clave = claveAnimalAnadir.value;
+    let valor = valorAnimalAnadir.value;
+
+    let datoAnadido = anadirDatoModelo(clave, valor);
+
+    if(!datoAnadido){
+        //Mostrar alerta
+        mostrarAlerta("El animal ya existe. No se puede añadir");
+    }
+
+    else{
+        anadirElementoVista(clave, valor);
+    }
+
+}
+
+function eliminarAnimal(){
+    
 }
